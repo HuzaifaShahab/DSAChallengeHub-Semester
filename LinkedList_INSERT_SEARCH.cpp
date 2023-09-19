775fccc027cfda53d9af4af205a4aaed2d31f65d
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-09-20 00:19:31 +0500

    Update LinkedList_INSERT_SEARCH.cpp

--- LinkedList_INSERT_SEARCH.cpp
+++ LinkedList_INSERT_SEARCH.cpp
@@ -17,12 +17,15 @@ void insertAtHead (int data , node * &Head){
 	node * newNode = new node (data);
 	newNode->next = Head ;
 	Head = newNode ;
+	
+	cout << data <<" as Head in list"<<endl;
 }
 
 void insertAtTail (int data , node * &Head){
  	node * newNode = new node (data) ;
  	if (Head == nullptr){
  		Head = newNode ;
+ 		cout << data << " as Tail in list" <<endl ;
  		return ;
 	 }
 	 
@@ -31,10 +34,12 @@ void insertAtTail (int data , node * &Head){
 	 	temp = temp->next ;
 	 }
 	 temp->next =  newNode ;	 
+	 cout << data << " as Tail in list" << endl ;
  }
  
  bool searchInLinkedList (node * Head , int key){
  	node * temp = Head ;
+ 	cout << key << " is the key" <<endl;
 	 	while (temp->next != nullptr){
 	 		if (temp->data == key) return true ;
 	 		temp = temp-> next ;
@@ -50,35 +55,53 @@ void insertAtTail (int data , node * &Head){
 		    cout << "null" << endl ;
 	 }				
 	 
+void delitionInHead (node * &head){
+//	if (head == nullptr){
+//		return ;
+//	}
+	
+	node * todelete = head ;
+	cout << head->data << " Head Deleted !" <<endl ;
+	head = head -> next ;
+	
+	delete todelete ;
+	
+}	 
 void delition (node * head,int value){
 	node * temp = head;
+	// n-1
+		while (temp->next->data != value){
+			temp = temp -> next ;
+		}
 	
-	while (temp->next->data != value){
-		temp = temp -> next ;
-	}
-	
-    node * todelete = temp->next ;
-    
-    temp->next = temp->next->next ;
+    node * todelete = temp->next ; // n
+    cout << temp->next->data << " Deleted Succesfully !"<<endl ;
+    temp->next = temp->next->next ;  // n+1
 	 delete todelete ;
 }
 int main (){
 	node * Head = nullptr;
 	
 	insertAtHead (2,Head);
+		display (Head) ;
 	insertAtHead (5,Head);
+		display (Head) ;
 	insertAtHead (6,Head);
 		display (Head) ;
 	insertAtTail (5,Head) ;
+		display (Head) ;
 	insertAtHead (78,Head);
 		display (Head) ;
-	bool found = searchInLinkedList (Head , 787) ;
+	
+		bool found = searchInLinkedList (Head , 787) ;
 	
 		if (found) cout << "Key has been found in list" << endl ;
 		else cout << "Key not in list!" << endl;
 		
 	delition (Head,5) ;
-	display(Head);	
+		display(Head);	
+	delitionInHead (Head);
+		display(Head);	
 		
 return 0 ;
 }
