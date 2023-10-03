4479b0fe90f8324f8d5efacc95ca9f8de428b01b
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-10-03 14:15:50 +0500

    Update Stack_Push_Pop_Peek_isStackEmpty.cpp

--- Stack_Push_Pop_Peek_isStackEmpty.cpp
+++ Stack_Push_Pop_Peek_isStackEmpty.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
+#include<stack>
 using namespace std ;
 
 class Stack {
-	int * array ;
-	int length ;
-	int top = -1 ;
+
+private:
+	int  * array ;
+	int size ;
+	int top=-1;
 	
-	public :
-	Stack (int length){
-		this->length = length ;
-		array = new int[length] ;	
+	public:
+	
+	Stack(int size){
+		array = new int[size] ;	
 	}
 	
 	void push (int element){
-		if (length - top > 1){
-			top++ ;
-			array[top] = element ;
+		if (size-top>1){
+			top++;
+			array[top] = element;
 		}
 		else{
-			cout << "Stack Overflow" <<endl;	
+			cout << "Stack Overflow" << endl ;	
 		}
 	}
 	
 	void pop (){
 		if (top >= 1){
-			array[top--] ;
+			array[top--];
 		}
 		else{
 			cout << "Stack is empty" << endl;
@@ -34,31 +37,37 @@ class Stack {
 	int peek (){
 		return array[top] ;
 }
-	bool isStackEmpty (){
+bool isStackEmpty (){
+
 		if (top <= -1){
 			return true ;
 		}
 		else  return false ;
 	}
 	
-	void displayStack (){
-		cout << "Elements in stack" <<endl;
+void displayStack (){
+	 cout << "Elements in stack" <<endl;
+		
 		for (int i=0 ; i<=top ; i++){
-			cout << array[i] << " " ;
+			cout <<" | " <<array[i] << " | " <<endl ;
+			cout << "____" <<endl;
 		}
+		
 	}
 };
 int main (){
+	
 	Stack stack(5) ;
-	stack.push(8);
+	stack.push(80);
 	stack.push(89);
 	stack.push(56) ;
 	stack.push(6) ;
-	cout << endl;
-	cout << stack.isStackEmpty () ;
-	cout << endl ;
-	cout << stack.peek () ;
-	cout << endl;
+
+
+	cout <<"Stack Status : " <<stack.isStackEmpty () ;
+
+	cout <<"Peek value is : "<<stack.peek() << endl;
+
 	stack.pop () ;
 	stack.displayStack () ;
 }
